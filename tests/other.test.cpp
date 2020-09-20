3bea8461165a2bb9c86ca1e9d28ec53104320365
szymmirr <szymmirr@wp.pl>
2020-09-20 22:41:44 +0200

    must fail

--- tests/other.test.cpp
+++ tests/other.test.cpp
@@ -2,7 +2,7 @@
 #include "string_utils.h"
 
 TEST_CASE("Succesful return", "[ehhhh]") {
-	REQUIRE(whatever() == 0);
+	REQUIRE(whatever() == 1);
 }
 
 TEST_CASE("Unuccesful return", "[ehhhh]") {
@@ -11,6 +11,6 @@ TEST_CASE("Unuccesful return", "[ehhhh]") {
 
 SCENARIO("two") {
     WHEN("three") {
-      REQUIRE(whatever() == 0);
+      REQUIRE(whatever() == 1);
     }
 }
